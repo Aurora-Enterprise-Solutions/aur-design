9393cfd79db5058b2f60ee9081da4cff6335c6f7
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2020-04-03 23:49:20 -0300

    [EN#1] Component Button v1.0.0

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <div class="buttons">
+      <div>
+        <au-button color="primary" :disabled="false" @click="buttonClick">Primary</au-button>
+      </div>
+      
+      <div style="margin-top: 10px">
+        <au-button color="hight" :disabled="false" @click="buttonClick">Hight Contrast</au-button>
+      </div>
+
+      <div style="margin-top: 10px">
+        <au-button :disabled="true" @click="buttonClick">Disabled</au-button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import AuButton from './components/AuButton'
+
+  export default {
+    name: 'App',
+    components: { AuButton },
+    methods: {
+      buttonClick(event) {
+        console.log('Button Click!', event);
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('./assets/styles/styles.css');
+</style>
---
+++ src/components/AuButton.vue
@@ -0,0 +1,92 @@
+<template>
+    <div class="au-button" :au-color="_color" :au-block="block">
+        <button class="au-button-element" :type="_htmlType" :disabled="disabled" @click="onClick($event)">
+            <!-- @slot Usa este slot para introducir el texto del botón -->
+            <slot></slot>
+        </button>
+    </div>
+</template>
+
+<script>
+    const COLORS = ['primary', 'hight']
+    const HTML_TYPE = ['button', 'submit', 'reset']
+
+    export default {
+        name: 'AuButton',
+        props: {
+            /**
+             * Color del botón.
+             * @values primary, hight
+             */
+            color: {
+                type: String,
+                required: false,
+                default: 'primary'
+            },
+
+            /**
+             * Icono del botón.
+             * Consulte los iconos de Aurora Icons.
+             */
+            icon: {
+                type: String,
+                required: false,
+                default: null
+            },
+
+            /**
+             * Tipo original de botón html (esto afecta a los formularios).
+             * @values button, submit, reset
+             */
+            htmlType: {
+                type: String,
+                required: false,
+                default: 'button'
+            },
+
+            /**
+             * Define si el botón se estirará al máximo ancho disponible o no.
+             */
+            block: {
+                type: Boolean,
+                required: false,
+                default: false
+            },
+
+            /**
+             * El botón se encuentra deshabilitado o no.
+             */
+            disabled: {
+                type: Boolean,
+                required: false,
+                default: false
+            }
+        },
+        data() {
+            return {
+                COLORS,
+                HTML_TYPE
+            }
+        },
+        computed: {
+            _color() {
+                return this.COLORS.includes(this.color) ? this.color : this.COLORS[0]
+            },
+
+            _htmlType() {
+                return this.HTML_TYPE.includes(this.htmlType) ? this.htmlType : this.HTML_TYPE[0]
+            },
+        },
+        methods: {
+            onClick(event) {
+                if (!this.disabled) {
+                    /**
+                     * Evento click.
+                     * @property {MouseEvent} event evento que emite el elemento button de html
+                     */
+                    this.$emit('click', event)
+                }
+            }
+        }
+    }
+</script>
