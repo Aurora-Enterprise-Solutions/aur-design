36d69b2f40dd6dd9e79f7641fbdf521ead1b2388
Jesus Barrientos <jesus.barrientosg.95@gmail.com>
2020-04-10 13:23:37 -0400

    Creado componente AuCard

---
+++ src/components/AuCard.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="au-card">
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'AuCard'
+}
+</script>
---
+++ tests/unit/card.spec.js
@@ -0,0 +1,14 @@
+import { mount, shallowMount } from '@vue/test-utils'
+import AuCard from '@/components/AuCard.vue'
+
+describe('Componente AuCard', () => {
+
+  // Valores por defecto de las props
+  const defaultData = {
+  }
+
+  test('el componente tiene nombre', () => {
+    const wrapper = mount(AuCard);
+    expect(wrapper.name()).toBe('AuCarde');
+  })
+})
